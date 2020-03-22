7392cfcff2f130bf952315a4db422b971630fd16
Michael Hueschen <michaelhueschen@gmail.com>
2020-03-21 18:09:15 -0700

    Use moving box UI; successfully sends coordinates to backend

--- backend/main.py
+++ backend/main.py
@@ -18,5 +18,9 @@ def test_message(message):
     print("received 'my event': " + str(message))
     emit('my response', {'data': 'got it!'})
 
+@socketio.on('coordinates')
+def coordinates(data):
+    print("received 'coordinates': " + str(data))
+
 if __name__ == '__main__':
     socketio.run(app)
